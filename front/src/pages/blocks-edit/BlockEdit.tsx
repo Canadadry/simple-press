9d487530a153e15106ee478cc3076acec0e0f675
Canadadry <moureyjerome-spam@yahoo.fr>
2025-12-12 21:39:40 +0100

    fix block edition

--- front/src/pages/blocks-edit/BlockEdit.tsx
+++ front/src/pages/blocks-edit/BlockEdit.tsx
@@ -6,32 +6,137 @@ import { useEffect, useState } from "react";
 import { Text, Flex, Spinner, Card } from "@radix-ui/themes";
 import { getBlockEdit, postBlockEdit } from "../../api/block";
 import type { Block } from "../../api/block";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 type SavingStatus = "untouched" | "touched" | "saving";
 
+interface NameProps {
+  tabIndex: number;
+  slug: string;
+  block: Block;
+  setBlock: (b: Block) => void;
+}
+
+function Name({ tabIndex, slug, block, setBlock }: NameProps) {
+  const [saving, setSaving] = useState<SavingStatus>("untouched");
+  const navigate = useNavigate();
+  return (
+    <Flex gap="3" mb="5">
+      <Box flexGrow="3">
+        <TextField.Root
+          tabIndex={tabIndex}
+          size="2"
+          placeholder="Title"
+          value={block.name}
+          disabled={saving === "saving"}
+          onChange={(e) => {
+            setSaving("touched");
+            setBlock({ ...block, name: e.target.value });
+          }}
+        >
+          <TextField.Slot>name</TextField.Slot>
+        </TextField.Root>
+      </Box>
+      <Button
+        tabIndex={tabIndex}
+        size="2"
+        disabled={saving != "touched"}
+        onClick={async () => {
+          setSaving("saving");
+          await postBlockEdit(slug, block);
+          setSaving("untouched");
+          navigate(`/blocks/${block.name}/edit`, { replace: true });
+        }}
+      >
+        {saving == "saving" ? <Spinner /> : "Save"}
+      </Button>
+    </Flex>
+  );
+}
+
+interface EditorProps {
+  tabIndex: number;
+  title: string;
+  content: string;
+  setContent: (content: string) => void;
+  updateContent: () => Promise<void>;
+}
+
+function Editor({
+  tabIndex,
+  title,
+  content,
+  setContent,
+  updateContent,
+}: EditorProps) {
+  const [saving, setSaving] = useState<SavingStatus>("untouched");
+  return (
+    <Box mb="4">
+      <Label htmlFor="skirt-description">
+        <Text size="2" weight="bold" mb="2" asChild>
+          <Box display="inline-block">{title}</Box>
+        </Text>
+      </Label>
+      <Box position="relative">
+        <TextArea
+          tabIndex={tabIndex}
+          spellCheck={false}
+          id="skirt-description"
+          variant="soft"
+          rows={10}
+          value={content}
+          style={{ paddingTop: 48 }}
+          disabled={saving === "saving"}
+          onChange={(e) => {
+            setSaving("touched");
+            setContent(e.target.value);
+          }}
+        />
+        <Box position="absolute" m="2" top="0" left="0" right="0">
+          <Flex gap={"1"}>
+            <Button
+              tabIndex={tabIndex}
+              size="2"
+              disabled={saving != "touched"}
+              onClick={async () => {
+                setSaving("saving");
+                await updateContent();
+                setSaving("untouched");
+              }}
+            >
+              {saving == "saving" ? <Spinner /> : "Save"}
+            </Button>
+          </Flex>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
 export default function BlockEdit() {
   const { slug } = useParams<{ slug: string }>();
-  const [blockName, setBlockName] = useState<string>("");
   const [block, setBlock] = useState<Block | null>(null);
-  const [saving, setSaving] = useState<SavingStatus>("untouched");
-  const tabIndex = 1;
+  const navidate = useNavigate();
   useEffect(() => {
     async function load() {
-      if (saving !== "untouched") {
-        return;
-      }
       if (!slug) {
         setBlock(null);
-        setBlockName("");
         return;
       }
       const res = await getBlockEdit(slug);
       setBlock(res);
-      setBlockName(res.name);
     }
     load();
-  }, [slug, saving]);
+  }, [slug]);
+
+  if (!slug) {
+    navidate("/", { replace: true });
+    return (
+      <Flex align="center" justify="center" height="100vh">
+        <Spinner />
+      </Flex>
+    );
+  }
 
   if (!block) {
     return (
@@ -44,122 +149,38 @@ export default function BlockEdit() {
   return (
     <Flex direction="column" gap="4">
       <Text size="7" weight="bold">
-        {blockName}
+        {slug}
       </Text>
       <Card>
         <Flex direction="column">
-          <Flex gap="3" mb="5">
-            <Box flexGrow="2">
-              <TextField.Root
-                tabIndex={tabIndex}
-                size="2"
-                placeholder="Title"
-                value={block.name}
-                disabled={saving === "saving"}
-                onChange={(e) => {
-                  setSaving("touched");
-                  setBlock({ ...block, name: e.target.value });
-                }}
-              >
-                <TextField.Slot>Nom</TextField.Slot>
-              </TextField.Root>
-            </Box>
-            <Button
-              tabIndex={tabIndex}
-              size="2"
-              disabled={saving != "touched"}
-              onClick={async () => {
-                setSaving("saving");
-                await postBlockEdit(blockName, block);
-                setSaving("untouched");
-              }}
-            >
-              {saving == "saving" ? <Spinner /> : "Save"}
-            </Button>
-          </Flex>
-          <Box mb="4">
-            <Label htmlFor="skirt-description">
-              <Text size="2" weight="bold" mb="2" asChild>
-                <Box display="inline-block">Content</Box>
-              </Text>
-            </Label>
-            <Box position="relative">
-              <TextArea
-                tabIndex={tabIndex}
-                spellCheck={false}
-                id="skirt-description"
-                variant="soft"
-                rows={10}
-                defaultValue={block.content || ""}
-                value={block.content}
-                style={{ paddingTop: 48 }}
-                disabled={saving === "saving"}
-                onChange={(e) => {
-                  setSaving("touched");
-                  setBlock({ ...block, content: e.target.value });
-                }}
-              />
-              <Box position="absolute" m="2" top="0" left="0" right="0">
-                <Flex gap={"1"}>
-                  <Button
-                    tabIndex={tabIndex}
-                    size="2"
-                    disabled={saving != "touched"}
-                    onClick={async () => {
-                      setSaving("saving");
-                      await postBlockEdit(block.name, block);
-                      setSaving("untouched");
-                    }}
-                  >
-                    {saving == "saving" ? <Spinner /> : "Save"}
-                  </Button>
-                </Flex>
-              </Box>
-            </Box>
-          </Box>
-          <Box mb="4">
-            <Label htmlFor="skirt-description">
-              <Text size="2" weight="bold" mb="2" asChild>
-                <Box display="inline-block">Content</Box>
-              </Text>
-            </Label>
-            <Box position="relative">
-              <TextArea
-                tabIndex={tabIndex}
-                spellCheck={false}
-                id="skirt-description"
-                variant="soft"
-                rows={10}
-                defaultValue={JSON.stringify(block.definition) || ""}
-                value={JSON.stringify(block.definition)}
-                style={{ paddingTop: 48 }}
-                disabled={saving === "saving"}
-                onChange={(e) => {
-                  setSaving("touched");
-                  setBlock({
-                    ...block,
-                    definition: JSON.parse(e.target.value),
-                  });
-                }}
-              />
-              <Box position="absolute" m="2" top="0" left="0" right="0">
-                <Flex gap={"1"}>
-                  <Button
-                    tabIndex={tabIndex}
-                    size="2"
-                    disabled={saving != "touched"}
-                    onClick={async () => {
-                      setSaving("saving");
-                      await postBlockEdit(block.name, block);
-                      setSaving("untouched");
-                    }}
-                  >
-                    {saving == "saving" ? <Spinner /> : "Save"}
-                  </Button>
-                </Flex>
-              </Box>
-            </Box>
-          </Box>
+          <Name
+            slug={slug}
+            tabIndex={1}
+            block={block}
+            setBlock={setBlock}
+          ></Name>
+          <Editor
+            tabIndex={2}
+            title="Content"
+            content={block.content}
+            setContent={(content: string) => {
+              setBlock({ ...block, content: content });
+            }}
+            updateContent={async () => {
+              await postBlockEdit(slug, block);
+            }}
+          ></Editor>
+          <Editor
+            tabIndex={3}
+            title="Data"
+            content={JSON.stringify(block.definition, null, 2)}
+            setContent={(content: string) => {
+              setBlock({ ...block, definition: JSON.parse(content) });
+            }}
+            updateContent={async () => {
+              await postBlockEdit(slug, block);
+            }}
+          ></Editor>
         </Flex>
       </Card>
     </Flex>
